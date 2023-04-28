4726c3a76b2817234b5bb298bc6519a2ed7c5772
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-04-28 21:57:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ st.title("Chat with Simon Wardley's Book")
 st.sidebar.markdown("# Query this book using AI")
 st.sidebar.markdown("Developed by Mark Craddock](https://twitter.com/mcraddock)", unsafe_allow_html=True)
 st.sidebar.markdown("Current Version: 0.1.4")
+st.sidebar.markdown("Using GPT-4 API)
 st.sidebar.markdown("Not optimised")
 st.sidebar.markdown("May run out of OpenAI credits")
 
