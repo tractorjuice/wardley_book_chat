2ede480c14503f9ef356592741fab3f56acda801
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-04-29 13:46:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ st.sidebar.markdown("Current Version: 0.1.4")
 st.sidebar.markdown("Using GPT-4 API")
 st.sidebar.markdown("Not optimised")
 st.sidebar.markdown("May run out of OpenAI credits")
+st.sidebar.markdown("Wardley Mapping is provided courtesy of Simon Wardley and licensed Creative Commons Attribution Share-Alike.")
 
 # Get datastore
 DATA_STORE_DIR = "data_store"
