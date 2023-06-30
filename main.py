4a398be01b9074a88b16f631c1286fa78def426c
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-06-30 12:53:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -36,37 +36,36 @@ st.sidebar.markdown("Wardley Mapping is provided courtesy of Simon Wardley and l
 DATA_STORE_DIR = "data_store"
 
 if "messages" not in st.session_state:
-    print("starting to do it")
     st.session_state.messages = []
 
-    if os.path.exists(DATA_STORE_DIR):
-        vector_store = FAISS.load_local(
-            DATA_STORE_DIR,
-            OpenAIEmbeddings()
-        )
-    else:
-        st.write(f"Missing files. Upload index.faiss and index.pkl files to {DATA_STORE_DIR} directory first")
-  
-    system_template="""Use the following pieces of context to answer the users question.
-    If you don't know the answer, just say that "I don't know", don't try to make up an answer.
-    ----------------
-    {summaries}"""
-    prompt_messages = [
-        SystemMessagePromptTemplate.from_template(system_template),
-        HumanMessagePromptTemplate.from_template("{question}")
-        ]
-    prompt = ChatPromptTemplate.from_messages(prompt_messages)
-    
-    chain_type_kwargs = {"prompt": prompt}
-    llm = ChatOpenAI(model_name=MODEL, temperature=0, max_tokens=300)  # Modify model_name if you have access to GPT-4
-    print("done chain stuff")
-    chain = RetrievalQAWithSourcesChain.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=vector_store.as_retriever(),
-        return_source_documents=True,
-        chain_type_kwargs=chain_type_kwargs
+if os.path.exists(DATA_STORE_DIR):
+    vector_store = FAISS.load_local(
+        DATA_STORE_DIR,
+        OpenAIEmbeddings()
     )
+else:
+    st.write(f"Missing files. Upload index.faiss and index.pkl files to {DATA_STORE_DIR} directory first")
+
+system_template="""Use the following pieces of context to answer the users question.
+If you don't know the answer, just say that "I don't know", don't try to make up an answer.
+----------------
+{summaries}"""
+prompt_messages = [
+    SystemMessagePromptTemplate.from_template(system_template),
+    HumanMessagePromptTemplate.from_template("{question}")
+    ]
+prompt = ChatPromptTemplate.from_messages(prompt_messages)
+
+chain_type_kwargs = {"prompt": prompt}
+llm = ChatOpenAI(model_name=MODEL, temperature=0, max_tokens=300)  # Modify model_name if you have access to GPT-4
+print("done chain stuff")
+chain = RetrievalQAWithSourcesChain.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vector_store.as_retriever(),
+    return_source_documents=True,
+    chain_type_kwargs=chain_type_kwargs
+)
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
