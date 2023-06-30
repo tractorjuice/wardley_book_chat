64c23173f5fd61418e9c113113fc1952ca96bc0a
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-06-30 12:46:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import openai
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQAWithSourcesChain
-from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.prompts.chat import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
 
 # Set OpenAI Model and API key
 openai.api_key = st.secrets["OPENAI_API_KEY"]
@@ -34,51 +34,37 @@ st.sidebar.markdown("Wardley Mapping is provided courtesy of Simon Wardley and l
 # Get datastore
 DATA_STORE_DIR = "data_store"
 
-if os.path.exists(DATA_STORE_DIR):
-  vector_store = FAISS.load_local(
-      DATA_STORE_DIR,
-      OpenAIEmbeddings()
-  )
-else:
-  st.write(f"Missing files. Upload index.faiss and index.pkl files to {DATA_STORE_DIR} directory first")
-  
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
-
-system_template="""Use the following pieces of context to answer the users question.
-Take note of the sources and include them in the answer in the format: "SOURCES: source1 source2", use "SOURCES" in capital letters regardless of the number of sources.
-If you don't know the answer, just say that "I don't know", don't try to make up an answer.
-----------------
-{summaries}"""
-prompt_messages = [
-    SystemMessagePromptTemplate.from_template(system_template),
-    HumanMessagePromptTemplate.from_template("{question}")
-    ]
-prompt = ChatPromptTemplate.from_messages(prompt_messages)
-
-chain_type_kwargs = {"prompt": prompt}
-llm = ChatOpenAI(model_name=MODEL, temperature=0, max_tokens=256)  # Modify model_name if you have access to GPT-4
-chain = RetrievalQAWithSourcesChain.from_chain_type(
-    llm=llm,
-    chain_type="stuff",
-    retriever=vector_store.as_retriever(),
-    return_source_documents=True,
-    chain_type_kwargs=chain_type_kwargs
-)
-
-#with st.spinner("Thinking..."):
-#    query = st.text_input("Question for the book?", value="What questions can I ask about this book?")
-#    result = chain(query)
-#    st.code (result)
-  
-#st.write("### Answer:")
-#st.write(result['answer'])
-
 if "messages" not in st.session_state:
     st.session_state.messages = []
+
+    if os.path.exists(DATA_STORE_DIR):
+        vector_store = FAISS.load_local(
+            DATA_STORE_DIR,
+            OpenAIEmbeddings()
+        )
+    else:
+        st.write(f"Missing files. Upload index.faiss and index.pkl files to {DATA_STORE_DIR} directory first")
+  
+    system_template="""Use the following pieces of context to answer the users question.
+    Take note of the sources and include them in the answer in the format: "SOURCES: source1 source2", use "SOURCES" in capital letters regardless of the number of sources.
+    If you don't know the answer, just say that "I don't know", don't try to make up an answer.
+    ----------------
+    {summaries}"""
+    prompt_messages = [
+        SystemMessagePromptTemplate.from_template(system_template),
+        HumanMessagePromptTemplate.from_template("{question}")
+        ]
+    prompt = ChatPromptTemplate.from_messages(prompt_messages)
+    
+    chain_type_kwargs = {"prompt": prompt}
+    llm = ChatOpenAI(model_name=MODEL, temperature=0, max_tokens=300)  # Modify model_name if you have access to GPT-4
+    chain = RetrievalQAWithSourcesChain.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vector_store.as_retriever(),
+        return_source_documents=True,
+        chain_type_kwargs=chain_type_kwargs
+    )
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
