808d4fda82b486c0ed7e912281b76ca371a82787
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-10-30 10:51:48 +0000

    Update app_faiss.py

--- app_faiss.py
+++ app_faiss.py
@@ -83,6 +83,15 @@ prompt_messages = [
     ]
 prompt = ChatPromptTemplate.from_messages(prompt_messages)
 
+if user_openai_api_key:
+    # If the user has provided an API key, use it
+    # Swap out openai for promptlayer
+    promptlayer.api_key = st.secrets["PROMPTLAYER"]
+    openai = promptlayer.openai
+    openai.api_key = user_openai_api_key
+else:
+    st.warning("Please enter your OpenAI API key", icon="⚠️")
+
 chain_type_kwargs = {"prompt": prompt}
 llm = PromptLayerChatOpenAI(
     model_name=MODEL,
