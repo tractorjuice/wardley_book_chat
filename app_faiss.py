0650ce9522467997ee632e5ac01c5c918c314e14
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2024-01-05 21:57:07 +0000

    Update app_faiss.py

--- app_faiss.py
+++ app_faiss.py
@@ -35,9 +35,6 @@ def clean_text(text):
     text = remove_markdown(text)
     return text
 
-if "session_id" not in st.session_state:
-    st.session_state.session_id = str(uuid.uuid4())
-
 st.set_page_config(page_title="Chat with Simon Wardley's Book")
 st.title("Chat with Simon Wardley's Book")
 st.sidebar.markdown("# Query Simon's book using AI")
@@ -48,77 +45,82 @@ st.sidebar.markdown(st.session_state.session_id)
 st.sidebar.markdown("Wardley Mapping is provided courtesy of Simon Wardley and licensed Creative Commons Attribution Share-Alike.")
 st.sidebar.divider()
 
+if "session_id" not in st.session_state:
+    st.session_state.session_id = str(uuid.uuid4())
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
 # Check if the user has provided an API key, otherwise default to the secret
 user_openai_api_key = st.sidebar.text_input("Enter your OpenAI API Key:", placeholder="sk-...", type="password")
 
-# Get datastore
-DATA_STORE_DIR = "data_store"
-
-if os.path.exists(DATA_STORE_DIR):
-    vector_store = FAISS.load_local(
-        DATA_STORE_DIR,
-        OpenAIEmbeddings()
-    )
-else:
-    st.write(f"Missing files. Upload index.faiss and index.pkl files to {DATA_STORE_DIR} directory first")
-
-custom_system_template="""
-    As a chatbot, analyze the provided book on Wardley Mapping and offer insights and recommendations.
-    Suggestions:
-    Explain the analysis process for a Wardley Map
-    Discuss the key insights derived from the book
-    Provide recommendations based on the analysis
-    Use the following pieces of context to answer the users question.
-    If you don't know the answer, just say that "I don't know", don't try to make up an answer.
-    Your primary objective is to help the user formulate excellent answers by utilizing the context of the specific service,
-    relevant details from your knowledge and insights from previous conversations.
-    ----------------
-    Reference Context and Knowledge from Similar Existing Services: {context}
-    Previous Conversations: {chat_history}"""
-
-custom_user_template = "Question:'''{question}'''"
-
-prompt_messages = [
-    SystemMessagePromptTemplate.from_template(custom_system_template),
-    HumanMessagePromptTemplate.from_template(custom_user_template)
-    ]
-prompt = ChatPromptTemplate.from_messages(prompt_messages)
-
 if user_openai_api_key:
+    
+    # Get datastore
+    DATA_STORE_DIR = "data_store"
+    
+    if os.path.exists(DATA_STORE_DIR):
+        vector_store = FAISS.load_local(
+            DATA_STORE_DIR,
+            OpenAIEmbeddings()
+        )
+    else:
+        st.write(f"Missing files. Upload index.faiss and index.pkl files to {DATA_STORE_DIR} directory first")
+    
+    custom_system_template="""
+        As a chatbot, analyze the provided book on Wardley Mapping and offer insights and recommendations.
+        Suggestions:
+        Explain the analysis process for a Wardley Map
+        Discuss the key insights derived from the book
+        Provide recommendations based on the analysis
+        Use the following pieces of context to answer the users question.
+        If you don't know the answer, just say that "I don't know", don't try to make up an answer.
+        Your primary objective is to help the user formulate excellent answers by utilizing the context of the specific service,
+        relevant details from your knowledge and insights from previous conversations.
+        ----------------
+        Reference Context and Knowledge from Similar Existing Services: {context}
+        Previous Conversations: {chat_history}"""
+    
+    custom_user_template = "Question:'''{question}'''"
+    
+    prompt_messages = [
+        SystemMessagePromptTemplate.from_template(custom_system_template),
+        HumanMessagePromptTemplate.from_template(custom_user_template)
+        ]
+    prompt = ChatPromptTemplate.from_messages(prompt_messages)
+    
     # If the user has provided an API key, use it
     # Swap out openai for promptlayer
     promptlayer.api_key = st.secrets["PROMPTLAYER"]
     openai = promptlayer.openai
     openai.api_key = user_openai_api_key
-else:
-    st.warning("Please enter your OpenAI API key", icon="⚠️")
-
-memory = ConversationBufferWindowMemory(memory_key="chat_history", return_messages=True, output_key='answer')
-
-llm = PromptLayerChatOpenAI(
-    model_name=MODEL,
-    temperature=0,
-    max_tokens=500,
-    pl_tags=["bookchat", st.session_state.session_id],
-)  # Modify model_name if you have access to GPT-4
-
-chain = ConversationalRetrievalChain.from_llm(
-    llm=llm,
-    retriever=vector_store.as_retriever(
-        search_kwargs={
-            "k": 3,
-            "score_threshold": .95,
-            }
-        ),
-    chain_type="stuff",
-    rephrase_question = True,
-    return_source_documents=True,
-    memory=memory,
-    combine_docs_chain_kwargs={'prompt': prompt}
-)
-   
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+    
+    memory = ConversationBufferWindowMemory(memory_key="chat_history", return_messages=True, output_key='answer')
+    
+    llm = PromptLayerChatOpenAI(
+        model_name=MODEL,
+        temperature=0,
+        max_tokens=500,
+        pl_tags=["bookchat", st.session_state.session_id],
+    )  # Modify model_name if you have access to GPT-4
+    
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vector_store.as_retriever(
+            search_kwargs={
+                "k": 3,
+                "score_threshold": .95,
+                }
+            ),
+        chain_type="stuff",
+        rephrase_question = True,
+        return_source_documents=True,
+        memory=memory,
+        combine_docs_chain_kwargs={'prompt': prompt}
+    )
+    
+    else:
+        st.warning("Please enter your OpenAI API key", icon="⚠️")
 
 for message in st.session_state.messages:
     if message["role"] in ["user", "assistant"]:
