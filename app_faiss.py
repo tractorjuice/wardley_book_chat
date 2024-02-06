a7c662e81a09a3ccd3412d651a60cd45c1bd897d
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2024-02-06 21:08:21 +0000

    Update app_faiss.py

--- app_faiss.py
+++ app_faiss.py
@@ -1,19 +1,21 @@
-import os, re, uuid
+import os
+import re
+import uuid
+from langchain_openai import OpenAI
 import promptlayer
 import streamlit as st
 from langchain_community.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain_openai import OpenAIEmbeddings
-from langchain_openai import OpenAI
 from langchain_community.chat_models import PromptLayerChatOpenAI
 from langchain_community.vectorstores import FAISS
 from langchain.prompts.chat import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
 
 # gpt-3.5-turbo, gpt-4, and gpt-4-turbo-preview point to the latest model version
 #MODEL = "gpt-3.5-turbo" # 4K, Sept 2021. Legacy. Currently points to gpt-3.5-turbo-0613.
-MODEL = "gpt-3.5-turbo-16k" # 16K, Sept 2021. Legacy. Snapshot of gpt-3.5-turbo from June 13th 2023. Will be deprecated on June 13, 2024
-#MODEL = "gpt-3.5-turbo-1106" # 16K, Sept 2021. New Updated GPT 3.5 Turbo. The latest GPT-3.5 Turbo model with improved instruction following, JSON mode, reproducible outputs, parallel function calling, and more. Returns a maximum of 4,096 output tokens.
+#MODEL = "gpt-3.5-turbo-16k" # 16K, Sept 2021. Legacy. Snapshot of gpt-3.5-turbo from June 13th 2023. Will be deprecated on June 13, 2024
+MODEL = "gpt-3.5-turbo-1106" # 16K, Sept 2021. New Updated GPT 3.5 Turbo. The latest GPT-3.5 Turbo model with improved instruction following, JSON mode, reproducible outputs, parallel function calling, and more. Returns a maximum of 4,096 output tokens.
 #MODEL = "gpt-4" # 8K, Sept 2021
 #MODEL = "gpt-4-32k" # 32K, Sept 2021
 #MODEL = "gpt-4-turbo-preview" # 128K, Apr 2023
@@ -65,18 +67,13 @@ user_openai_api_key = st.sidebar.text_input("Enter your OpenAI API Key:", placeh
 DATA_STORE_DIR = "data_store"
 
 if user_openai_api_key:
-    
-    # If the user has provided an API key, use it
-    # Swap out openai for promptlayer
-    promptlayer.api_key = st.secrets["PROMPTLAYER"]
-    openai = promptlayer.openai
-    openai.api_key = user_openai_api_key
+    os.environ["OPENAI_API_KEY"] = user_openai_api_key
     
     if "vector_store" not in st.session_state:
         if os.path.exists(DATA_STORE_DIR):
             st.session_state.vector_store = FAISS.load_local(
                 DATA_STORE_DIR,
-                OpenAIEmbeddings(openai_api_key=user_openai_api_key)
+                OpenAIEmbeddings()
             )
         else:
             st.write(f"Missing files. Upload index.faiss and index.pkl files to {DATA_STORE_DIR} directory first")
@@ -103,13 +100,19 @@ if user_openai_api_key:
             HumanMessagePromptTemplate.from_template(custom_user_template)
             ]
         prompt = ChatPromptTemplate.from_messages(prompt_messages)
+        
+        # If the user has provided an API key, use it
+        # Swap out openai for promptlayer
+        promptlayer.api_key = st.secrets["PROMPTLAYER"]
+        openai = promptlayer.openai
+        openai.api_key = user_openai_api_key
     
     if "memory" not in st.session_state:
         st.session_state.memory = ConversationBufferWindowMemory(memory_key="chat_history", return_messages=True, output_key='answer')
     
     if "llm" not in st.session_state:
         st.session_state.llm = PromptLayerChatOpenAI(
-            model=MODEL,
+            model_name=MODEL,
             temperature=0,
             max_tokens=300,
             pl_tags=["bookchat", st.session_state.session_id],
@@ -130,32 +133,31 @@ if user_openai_api_key:
             memory=st.session_state.memory,
             combine_docs_chain_kwargs={'prompt': prompt}
         )
-
+    
+    for message in st.session_state.messages:
+        if message["role"] in ["user", "assistant"]:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+    
+    if query := st.chat_input("What question do you have for the book?"):
+        st.session_state.messages.append({"role": "user", "content": query})
+        with st.chat_message("user"):
+            st.markdown(query)
+    
+        with st.spinner():
+            with st.chat_message("assistant"):
+                response = st.session_state.chain(query)
+                st.markdown(response['answer'])
+                st.divider()
+                
+                source_documents = response['source_documents']
+                for index, document in enumerate(source_documents):
+                    if 'source' in document.metadata:
+                        source_details = document.metadata['source']
+                        cleaned_content = clean_text(document.page_content)
+                        st.warning(f"Source {index + 1}: Page {document.metadata['page']}\n")
+                        st.write(f"{cleaned_content}\n")
+    
+            st.session_state.messages.append({"role": "assistant", "content": response['answer']})
 else:
     st.warning("Please enter your OpenAI API key", icon="⚠️")
-  
-for message in st.session_state.messages:
-    if message["role"] in ["user", "assistant"]:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-if query := st.chat_input("What question do you have for the book?"):
-    st.session_state.messages.append({"role": "user", "content": query})
-    with st.chat_message("user"):
-        st.markdown(query)
-
-    with st.spinner():
-        with st.chat_message("assistant"):
-            response = st.session_state.chain(query)
-            st.markdown(response['answer'])
-            st.divider()
-            
-            source_documents = response['source_documents']
-            for index, document in enumerate(source_documents):
-                if 'source' in document.metadata:
-                    source_details = document.metadata['source']
-                    cleaned_content = clean_text(document.page_content)
-                    st.warning(f"Source {index + 1}: Page {document.metadata['page']}\n")
-                    st.write(f"{cleaned_content}\n")
-
-        st.session_state.messages.append({"role": "assistant", "content": response['answer']})
